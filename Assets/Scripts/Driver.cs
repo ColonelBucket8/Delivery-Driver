649682db66e3c304912cf78ed906c86a936c8ef6
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-01 15:38:24 +0800

    Add rotation and translation to the car

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
@@ -7,12 +5,14 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 0.1f);
+        transform.Translate(0, 0.01f, 0);
+
     }
 }
