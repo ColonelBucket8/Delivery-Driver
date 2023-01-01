9c4694006c7a0cb3e488817e69e5ff0e0a223d88
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-02 01:56:11 +0800

    Destroy package when picked up and only one package can be carried at one time

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
     private bool hasPackage = false;
     void OnCollisionEnter2D(Collision2D other)
     {
@@ -11,10 +12,11 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         // Package
-        if (other.CompareTag("Package"))
+        if (other.CompareTag("Package") && !hasPackage)
         {
             Debug.Log("Package picked up");
             hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
         }
 
         if (other.CompareTag("Customer") && hasPackage)
