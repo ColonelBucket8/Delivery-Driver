23e2363ccb9bf144998d70f250a5cf1b77d2fe19
ColonelBucket8 <59568628+ColonelBucket8@users.noreply.github.com>
2023-01-02 11:20:47 +0800

    FEATURE: Add boost and bump mechanics 1. Boost car speed when picking up boost item 2. Slow car speed when bumping with other collider objects

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 20f;
     [SerializeField] float steerSpeed = 200f;
+    [SerializeField] private float slowSpeed = 15f;
+    [SerializeField] private float boostSpeed = 30f;
+    [SerializeField] private float resetSpeed = 20f;
 
     void Start()
     {
@@ -18,6 +21,30 @@ public class Driver : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
 
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bump"))
+        {
+            Debug.Log("Bumping into object");
+            moveSpeed = slowSpeed;
+            Invoke(nameof(ResetSpeed), 5);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Boost"))
+        {
+            moveSpeed = boostSpeed;
+            Invoke(nameof(ResetSpeed), 5);
+        }
+    }
+
+    void ResetSpeed()
+    {
+        moveSpeed = resetSpeed;
     }
 
 
